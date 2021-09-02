8db25d359f162912f5fc8ef851c108dd6b90db10
ravi-neosoftmail <ravi.ranjan@neosoftmail.com>
2021-09-02 11:54:18 +0530

    first commit

---
+++ src/containers/navigation/StackNavigation/StackNavigation.js
@@ -0,0 +1,17 @@
+// import React from 'react';
+// import {createNativeStackNavigator} from '@react-navigation/native-stack';
+// import {NavigationContainer} from '@react-navigation/native';
+// import StackNavigation from '../StackNavigator/StackNavigator';
+
+// const Stack = createNativeStackNavigator();
+
+// export default function StackNavigation() {
+ 
+//   return (
+//     <NavigationContainer>
+//       <Stack.Navigator>
+//         <Stack.Screen name="WhatsApp" component={Home} />
+//       </Stack.Navigator>
+//     </NavigationContainer>
+//   );
+// }
